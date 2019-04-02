441c24592f3dfd9ac4af7cea59e14f3bc44f9f84
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-04-03 03:03:30 +0600

    Add scroll to top button

--- js/resume.js
+++ js/resume.js
@@ -43,4 +43,14 @@
     $.magnificPopup.close();
   });
 
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
 })(jQuery); // End of use strict
