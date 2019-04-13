3d01b3bb2bc9c1628995bf942eb0b5d9e621e69b
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-04-14 04:00:41 +0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,11 @@ var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var csso = require('gulp-csso');
+var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
-
-// Image compression start
 var imagemin = require('gulp-imagemin');
 var imageminPngquant = require('imagemin-pngquant');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
-// Image compression end
 
 // Set browser(s) to support autoprefixer start
 var AUTOPREFIXER_BROWSERS = [
@@ -31,19 +28,16 @@ var AUTOPREFIXER_BROWSERS = [
 	'bb >= 10'
 ];
 
-// File paths start
-var CSS_PATH = 'public/css/**/*.css';
+// File source paths start
+var CSS_PATH = 'src/styles/**/*.css';
+var SCRIPTS_PATH = 'src/scripts/**/*.js';
+var IMAGES_PATH = 'src/images/**/*.{png,jpeg,jpg,svg,gif}';
+
+// var DIST_PATH = 'public';
+var DIST_PATH = '_test';
+// File source paths end
 
 
-var SCRIPTS_PATH = 'public/scripts/**/*.js';
-
-var IMAGES_PATH = 'public/images/**/*.{png,jpeg,jpg,svg,gif}';
-
-var DIST_PATH = 'public';
-// File paths end
-
-
-// Gulp tasks
 
 // Default task
 gulp.task('default', ['clean', 'images', 'styles', 'scripts'], function() {});
@@ -66,32 +60,13 @@ gulp.task('styles', function() {
 		}))
 		.pipe(sourcemaps.init())
 		.pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS })) // add vendor prefixes
-		.pipe(concat('styles.css')) // concat all the css files
-		.pipe(csso()) // minify all the css file(s)
+		.pipe(concat('resume.min.css')) // concat all the css files
+		.pipe(cleanCSS()) // minify all the css file(s)
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(DIST_PATH)) // new minified css file location
+		.pipe(gulp.dest(DIST_PATH + '/styles')) // new minified css file location
 		.pipe(livereload()) // check for updates
 	);
 });
-
-
-// Styles for SCSS task
-// gulp.task('styles', function() {
-//     return (
-//         gulp.src('./public/scss/styles.scss') // path for source scss file
-//         .pipe(plumber(function(err) { // Restart server if any error occurs
-//             console.log('Styles Task Error:');
-//             console.log(err);
-//             this.emit('end');
-//         }))
-//         .pipe(sourcemaps.init())
-//         .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS })) // add vendor prefixes
-//         .pipe(sass({ outputStyle: 'compressed' })) // minify all the scss file(s)
-//         .pipe(sourcemaps.write())
-//         .pipe(gulp.dest(DIST_PATH)) // new minified scss to css file location
-//         .pipe(livereload()) // check for updates
-//     );
-// });
 
 
 // Scripts task
@@ -106,9 +81,9 @@ gulp.task('scripts', function() {
 		.pipe(sourcemaps.init())
 		.pipe(babel({ presets: ['@babel/env'] })) // transpile all script files to es5
 		.pipe(uglify()) // minify all the script files
-		.pipe(concat('scripts.js')) // concat all the script files
+		.pipe(concat('scripts.min.js')) // concat all the script files
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(DIST_PATH)) // new minified script file location
+		.pipe(gulp.dest(DIST_PATH + '/sripts')) // new minified script file location
 		.pipe(livereload()) // check for updates
 	);
 });
@@ -118,16 +93,17 @@ gulp.task('scripts', function() {
 gulp.task('images', function() {
 	return (
 		gulp.src(IMAGES_PATH) // path for source image files
-		// .pipe(imagemin()) // lossless compression
-		.pipe(imagemin([
-			imagemin.gifsicle(),
-			imagemin.jpegtran(),
-			imagemin.optipng(),
-			imagemin.svgo(),
-			imageminPngquant(),
-			imageminJpegRecompress()
-		])) // lossy compression
+		.pipe(imagemin()) // lossless compression
+		// .pipe(imagemin([
+		// 	imagemin.gifsicle(),
+		// 	imagemin.jpegtran(),
+		// 	imagemin.optipng(),
+		// 	imagemin.svgo(),
+		// 	imageminPngquant(),
+		// 	imageminJpegRecompress()
+		// ])) // lossy compression
 		.pipe(gulp.dest(DIST_PATH + '/images')) // new compressed image file(s) location
+		.pipe(livereload()) // check for updates
 	);
 });
 
@@ -139,7 +115,6 @@ gulp.task('watch', ['default'], function() {
 	livereload.listen(); // listen for any change
 
 	gulp.watch(CSS_PATH, ['styles']); // run `styles` task when any change occurs in CSS_PATH
-	// gulp.watch(SCSS_PATH, ['styles']); // run `styles` task when any change occurs in SCSS_PATH
-
 	gulp.watch(SCRIPTS_PATH, ['scripts']); // run `scripts` task when any change occurs in SCRIPTS_PATH
+	gulp.watch(IMAGES_PATH, ['images']); // run `images` task when any change occurs in IMAGES_PATH
 });
