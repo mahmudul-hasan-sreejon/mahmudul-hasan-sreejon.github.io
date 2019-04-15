85775ed53556669cbed55dda768d8fea9dcfb42e
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-04-15 17:53:38 +0600

    Code cleanup

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var StaticServer = require('static-server');
 
 
 var server = new StaticServer({
-    rootPath: './',
-    port: 3000
+  rootPath: './',
+  port: 3000
 });
 
 server.start(function() {
-    console.log('App running at: http://localhost:' + server.port + '/');
-    console.log('Type Control-C twice to stop.\n');
+  console.log('App running at: http://localhost:' + server.port + '/');
+  console.log('Type Control-C twice to stop.\n');
 });
